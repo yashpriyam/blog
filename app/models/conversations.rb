c6e7774bcc9e1555ba861d938c56e893127b4f8a
yashpriyam <yashpriyam74@gmail.com>
2019-07-13 18:29:12 +0530

    lastly

--- app/models/conversations.rb
+++ app/models/conversations.rb
@@ -1,4 +1,4 @@
-class Conversation < ActiveRecord::Base
+class Conversation < ApplicationRecord
   belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
   belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
 
