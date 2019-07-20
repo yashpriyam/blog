f80c804ee3a3fc08f995ecb65b53d2e6c6dd498a
yashpriyam <yashpriyam74@gmail.com>
2019-07-20 08:54:41 +0530

    removed unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-  resources :conversations do
-    resources :messages
-  end
-
+    
   root 'posts#index'
-  root 'conversations#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
